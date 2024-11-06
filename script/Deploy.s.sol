e7c62f9a24e821cc8af13a4aa17332c707180f91
Vitor <marthendalnunes@gmail.com>
2024-11-06 13:53:58 -0300

    feat: deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,10 +2,23 @@
 pragma solidity >=0.8.23 <0.9.0;
 
 import { BaseScript } from "./Base.s.sol";
+import { Document } from "../src/Document.sol";
+import { Identifier } from "../src/Identifier.sol";
+import { Resolver } from "../src/Resolver.sol";
 
-/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-// function run() public broadcast returns (Foo foo) {
-//     // foo = new Foo();
-// }
+    uint256 private deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    function run() public returns (Document document, Resolver resolver) {
+        vm.startBroadcast(deployerPrivateKey);
+        // Deploy document contract
+        document = new Document();
+
+        // Deploy identifier and resolver implementations
+        Identifier identifierImplementation = new Identifier();
+
+        // Deploy resolver contract
+        resolver = new Resolver(msg.sender, address(identifierImplementation), "http://localhost:8787");
+        vm.stopBroadcast();
+    }
 }
