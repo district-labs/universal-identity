733dbec093c8d0f7cb52d5152f53ae7b724a5376
Vitor Marthendal Nunes <marthendalnunes@gmail.com>
2024-12-02 17:24:24 -0300

    feat: CREATE2 deploymets with safe singleton (#4)

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,24 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.23 <0.9.0;
 
+import { SafeSingletonDeployer } from "safe-singleton-deployer-sol/SafeSingletonDeployer.sol";
 import { BaseScript } from "./Base.s.sol";
 import { Document } from "../src/Document.sol";
 import { Identifier } from "../src/Identifier.sol";
 import { UniversalResolver } from "../src/UniversalResolver.sol";
 
 contract Deploy is BaseScript {
+    bytes32 salt = vm.envBytes32("SALT");
+    string resolverUrl = vm.envString("RESOLVER_URL");
     uint256 private deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
     function run() public returns (Document document, UniversalResolver resolver) {
         vm.startBroadcast(deployerPrivateKey);
         // Deploy document contract
-        document = new Document();
+        document = Document(SafeSingletonDeployer.deploy({ creationCode: type(Document).creationCode, salt: salt }));
 
         // Deploy identifier implementation
-        Identifier identifierImplementation = new Identifier();
+        Identifier identifierImplementation =
+            Identifier(SafeSingletonDeployer.deploy({ creationCode: type(Identifier).creationCode, salt: salt }));
 
         // Deploy Universal Resolver
-        resolver = new UniversalResolver(msg.sender, address(identifierImplementation), vm.envString("RESOLVER_URL"));
+        resolver = UniversalResolver(
+            SafeSingletonDeployer.deploy({
+                creationCode: type(UniversalResolver).creationCode,
+                args: abi.encode(msg.sender, address(identifierImplementation), resolverUrl),
+                salt: salt
+            })
+        );
         vm.stopBroadcast();
     }
 }
